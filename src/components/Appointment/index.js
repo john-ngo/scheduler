f96d8480bdeb9dd1c61c76d0d08da9f92a94260c
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-02-27 22:11:34 -0700

    Add edit appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -16,6 +16,7 @@ export default function Appointment(props) {
   const SAVING = "SAVING";
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -49,17 +50,27 @@ export default function Appointment(props) {
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === CREATE && (
         <Form
           interviewers={props.interviewers}
-          onCancel={() => back()}
+          onCancel={back}
+          onSave={save}
+        />
+      )}
+      {mode === EDIT && (
+        <Form
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onCancel={back}
           onSave={save}
         />
       )}
       {mode === SAVING && (
-        <Status />
+        <Status message="Saving" />
       )}
       {mode === DELETING && (
         <Status message="Deleting" />
@@ -67,7 +78,7 @@ export default function Appointment(props) {
       {mode === CONFIRM && (
         <Confirm
           message="Are you sure you would like to delete?"
-          onCancel={() => transition(SHOW)}
+          onCancel={back}
           onConfirm={cancel}
         />
       )}
